87684a358f6bf5add33faf198999f4ac4e025297
Greg Mac <manov@manov.ru>
2018-05-13 09:48:53 +0300

    Menu Animation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 headerToggle.onclick = function expandMenu() {
 
   var headerNavList = document.getElementById('headerNavList');
+  var headerTop = document.getElementById('headerTop');
 
   let headerNavListClass = headerNavList.classList;
+  let headerTopClass = headerTop.classList;
 
   // console.log(headerNavClass);
+      console.log(headerNavList.style.display);
+  if (headerNavList.style.display === "none" || headerNavList.style.display === "") {
 
-  // if (headerNavListClass.className === "header-top__nav") {
-      headerNavList.style.display = "none";
-  // } else {
-    // headerNavListClass.remove("responsive");
-  // }
+      headerNavList.style.display = "block";
+      headerNavList.style.opacity = "1";
+      console.log(headerNavList.style.opacity);
+      headerTop.style.paddingBottom = "145px";
+
+      console.log(headerNavList.style.display);
+  } else {
+    headerNavList.style.display = "none";
+    headerTop.style.paddingBottom = "0px";
+
+    }
+
 
 
 }
